8fb55213c022f9b0e2283ae24f75fa74f86e9a7c
최하영 <hychoi318@gmail.com>
2022-07-23 23:25:42 +0900

    add report 페이지 추가 및 구현

--- projects/02_all_about_jobs/app.py
+++ projects/02_all_about_jobs/app.py
@@ -1,12 +1,58 @@
-from flask import Flask, render_template
+# from os import sendfile
+from flask import Flask, redirect, render_template, request, send_file
+from scrapping import get_jobs
+from exporter import save_to_file
+
 app = Flask(__name__)
+
+fake_db = {}
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/<username>')
-def contact(username):
-    return f'Hello {username} how ya doin'
+@app.route('/report')
+def contact():
+    keyword = request.args.get("keyword")
+
+    if keyword:
+        keyword = keyword.lower()
+        existingJobs = fake_db.get(keyword)
+
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(keyword)
+            fake_db[keyword] = jobs
+    else:
+        return redirect("/")
+        # return render_template("home.html")
+
+    return render_template(
+        "report.html",
+        searchingBy = keyword,
+        resultsNum = len(jobs),
+        jobs = jobs
+    )
+
+@app.route("/export")
+def export():
+    try:
+        keyword = request.args.get("keyword")
+
+        if not keyword:
+            raise Exception()
+
+        keyword = keyword.lower()
+        jobs = fake_db.get(keyword)
+
+        if not jobs:
+            raise Exception()
+
+        save_to_file(jobs)
+        return send_file("your_jobs.csv", as_attachment=True)
+
+    except:
+        return redirect("/")
 
 # app.run(host='0.0.0.0')
---
+++ projects/02_all_about_jobs/scrapping.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 15
+
+def get_last_page(URL):
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("ul", {"class": "pagination-list"})
+    pages = []
+    
+    # links = pagination.find_all("a")
+
+    # for link in links[:-1]:
+    #     pages.append(int(link.string))
+    
+    # max_page = pages[-1]
+
+    if pagination:
+        links = pagination.find_all("a")
+
+        for link in links[:-1]:
+            pages.append(int(link.string))
+        
+        max_page = pages[-1]
+    else:
+        max_page = 1
+
+    return max_page
+
+def extract_job(html):
+    title = html.find("h2", {"class": "jobTitle"})
+    company = html.find("span", {"class": "companyName"})
+    location = html.find("div", {"class": "companyLocation"})
+    job_id = ""
+    
+    if (title != None):
+        job_id = title.find("a")["data-jk"]
+        title = title.find("a")["aria-label"]
+        company_anchor = company.find("a")
+        
+        if (company_anchor != None):
+            company = company_anchor.string
+        else:
+            company = company.string
+
+        company = company.strip()
+        location = location.string
+        
+    return {
+        "title": title, 
+        "company": company, 
+        "location": location, 
+        "link": f"https://kr.indeed.com/viewjob?jk={job_id}"
+    }
+
+def extract_jobs(last_page, URL):
+    jobs = []
+    
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        
+        result = requests.get(f"{URL}&start={page * LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        result_list = soup.find("ul", {"class" : "jobsearch-ResultsList"})
+        results = result_list.find_all("li")
+    
+        for result in results:
+            job = extract_job(result)
+        
+            if (job['title'] != None):
+                jobs.append(job)
+    
+    return jobs
+
+def get_jobs(keyword):
+    URL = f"https://kr.indeed.com/jobs?q={keyword}&l=&from=searchOnHP"
+    last_page = get_last_page(URL)
+    jobs = extract_jobs(last_page, URL)
+
+    return jobs
